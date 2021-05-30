8eb6d4b19f7bcc1b858da67cc9ffff2a6d9bb5e9
wiehajster <natalia.s.299269@gmail.com>
2021-05-30 10:10:07 +0200

    projekt hurtowni

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 import time
+import datetime
 import random
 
 from sqlalchemy import create_engine
@@ -20,6 +21,44 @@ def add_new_row(n):
         str(n) + "," + 
         str(int(round(time.time() * 1000))) + ");")
 
+def add_new_transaction_row(market, timestamp, quantity, price, total, order_type):
+    day_of_week = datetime.datetime.strptime("2021-05-29T17:06:20.08", '%Y-%m-%dT%H:%M:%S.%f').strftime('%A')
+    date = datetime.datetime.strptime("2021-05-29T17:06:20.08", '%Y-%m-%dT%H:%M:%S.%f').strftime('%Y-%m-%d')
+    day = datetime.datetime.strptime("2021-05-29T17:06:20.08", '%Y-%m-%dT%H:%M:%S.%f').strftime('%d')
+    month = datetime.datetime.strptime("2021-05-29T17:06:20.08", '%Y-%m-%dT%H:%M:%S.%f').strftime('%B')
+    year = datetime.datetime.strptime("2021-05-29T17:06:20.08", '%Y-%m-%dT%H:%M:%S.%f').strftime('%Y')
+    month_number = datetime.datetime.strptime("2021-05-29T17:06:20.08", '%Y-%m-%dT%H:%M:%S.%f').strftime('%m')
+    quarter = (int(month_number)-1)//3 + 1
+    
+    last_id = db.execute("INSERT INTO TransactionTime (t_date, t_day, t_month, t_year, quarter, day_of_week) "+
+        "VALUES ("+ "'"+
+        date + "'," +
+        str(day) + "," + "'" +
+        str(month) + "'," +
+        str(year) + "," +
+        str(quarter) + "," + "'" +
+        str(day_of_week) + "') RETURNING id_time;")
+
+    id_market = db.execute("SELECT id_market FROM Market WHERE market_name = '" + market + "';")
+    id_market = next(iter(id_market))[0]
+    id_type = db.execute("SELECT id_type FROM TransactionType WHERE t_type_name = '" + order_type + "';")
+    id_type = next(iter(id_type))[0]
+    id_time = next(iter(last_id))[0]
+    '''
+    for l, m, t in zip(last_id, id_market, id_type):
+        id_time = l[0]
+        id_market = m[0]
+        id_type = t[0]
+    '''
+    db.execute("INSERT INTO CryptoTransaction (id_time, id_market, id_type, quantity, price, total) "+
+        "VALUES ("+
+        str(id_time) + "," +
+        str(id_market) + "," +
+        str(id_type) + "," + 
+        str(quantity) + "," +
+        str(price) + "," +
+        str(total) + ");")
+
 def get_last_row():
     # Retrieve the last number inserted inside the 'numbers'
     query = "" + \
@@ -34,8 +73,26 @@ def get_last_row():
 
 if __name__ == '__main__':
     print('Application started')
-
+    '''
     while True:
         add_new_row(random.randint(1,100000))
         print('The last value insterted is: {}'.format(get_last_row()))
         time.sleep(5)
+    '''
+    
+    add_new_transaction_row('BTC-DOGE', "2014-07-09T03:21:20.08", 0.30802438, 0.012634, 0.00389158, 'BUY')
+    add_new_transaction_row('BTC-DOGE', "2014-07-10T03:21:20.08", 0.50802438, 0.112634, 0.20389158, 'BUY')
+    add_new_transaction_row('BTC-DOGE', "2014-07-11T03:21:20.08", 0.50802438, 0.112634, 0.20389158, 'SELL')
+    add_new_transaction_row('USD-BTC', "2014-07-10T03:21:20.08", 0.50222438, 0.14534, 0.203589158, 'BUY')
+    
+    query = "SELECT c.id_time, c.id_type, c.id_market " + \
+            "FROM CryptoTransaction c " + \
+            "JOIN TransactionTime t ON t.id_time=c.id_time " + \
+            "JOIN Market m ON m.id_market=c.id_market " + \
+            "JOIN TransactionType tt ON tt.id_type=c.id_type " + \
+            "WHERE m.market_name='BTC-DOGE'"
+    
+    results = db.execute(query)
+    
+    for r in results:
+        print(r)
--- warehouse/init.sql
+++ warehouse/init.sql
@@ -1,4 +1,96 @@
+/*
 CREATE TABLE IF NOT EXISTS numbers (
     number BIGINT,
     timestamp BIGINT
+);
+*/
+
+CREATE TABLE IF NOT EXISTS TransactionTime (
+    id_time SERIAL PRIMARY KEY,
+    t_date DATE NOT NULL,
+    t_day INT NOT NULL,
+    t_month VARCHAR(15) NOT NULL, --można zamienić na int
+    t_year INT NOT NULL,
+    quarter INT NOT NULL,
+    day_of_week VARCHAR(15) NOT NULL --można zamienić na int
+);
+
+CREATE TABLE IF NOT EXISTS Market (
+    id_market SERIAL PRIMARY KEY,
+    market_name VARCHAR(15) NOT NULL
+);
+
+--przykład
+INSERT INTO Market(market_name) SELECT 'BTC-DOGE'
+WHERE NOT EXISTS (
+    SELECT 1 FROM Market WHERE market_name='BTC-DOGE'
+);
+INSERT INTO Market(market_name) SELECT 'USD-BTC'
+WHERE NOT EXISTS (
+    SELECT 1 FROM Market WHERE market_name='USD-BTC'
+);
+
+CREATE TABLE IF NOT EXISTS TransactionType (
+    id_type SERIAL PRIMARY KEY,
+    t_type_name varchar(10) NOT NULL
+);
+
+--przykład
+INSERT INTO TransactionType(t_type_name) SELECT 'SELL'
+WHERE NOT EXISTS (
+    SELECT 1 FROM TransactionType WHERE t_type_name='SELL'
+);
+
+INSERT INTO TransactionType(t_type_name) SELECT 'BUY'
+WHERE NOT EXISTS (
+    SELECT 1 FROM TransactionType WHERE t_type_name='BUY'
+);
+
+CREATE TABLE IF NOT EXISTS CryptoTransaction (
+    id_time INT REFERENCES TransactionTime(id_time) NOT NULL,
+    id_market INT REFERENCES Market(id_market) NOT NULL,
+    id_type INT REFERENCES TransactionType(id_type) NOT NULL,
+    PRIMARY KEY (id_time, id_market, id_type),
+    quantity REAL NOT NULL,
+    price REAL NOT NULL,
+    total REAL NOT NULL
+);
+
+/*
+możliwe, że można wykorzystać tabelę TransactionTime
+do przechowywania tych czasów zamiast tworzyć nową
+*/
+CREATE TABLE IF NOT EXISTS EquityTime (
+    id_time SERIAL PRIMARY KEY,
+    t_date DATE NOT NULL,
+    t_day VARCHAR(15) NOT NULL,
+    t_month VARCHAR(15) NOT NULL,
+    t_year INT NOT NULL,
+    quarter INT NOT NULL,
+    day_of_week INT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Company (
+    id_company SERIAL PRIMARY KEY,
+    company_name VARCHAR(40) NOT NULL,
+    isin VARCHAR(40) NOT NULL --nie wiem czy potrzebne
+);
+
+/*
+nazwy kolumn brałam stąd https://www.gpw.pl/price-archive?fetch=0&type=10&instrument=&date=28-05-2021&show_x=Show+results
+nie ma tam ostatnich kolumn, które znajdują się w polskiej wersji
+*/
+
+CREATE TABLE IF NOT EXISTS Equity (
+    id_time INT REFERENCES EquityTime(id_time) NOT NULL,
+    id_company INT REFERENCES Company(id_company) NOT NULL,
+    PRIMARY KEY (id_time, id_company),
+    opening_price REAL NOT NULL,
+    maximum_price REAL NOT NULL,
+    minimum_price REAL NOT NULL,
+    closing_price REAL NOT NULL,
+    percent_price_change REAL NOT NULL,
+    trade_volume REAL NOT NULL,
+    transactions_number INT NOT NULL,
+    turnover_value REAL NOT NULL
 );
